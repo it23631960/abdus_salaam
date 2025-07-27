a965feb70f84d7f3f23a968a47efab5b91658acc
it23631960 <it23631960@my.sliit.lk>
2025-07-27 10:11:11 +0530

    hasiths update 27

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'testuser' );
+define( 'DB_NAME', 'wordpress_db' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
